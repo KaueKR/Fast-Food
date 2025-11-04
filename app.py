9ca558b82c1cb278fa7a18668b7246bdb8b72351
KaueKR <kauekruiz@gmail.com>
2025-11-04 17:26:01 -0300

    Creation of the main menu

---
+++ app.py
@@ -0,0 +1,12 @@
+def show_options_of_the_main_menu():
+    print("1. Register restaurant")
+    print("2. List restaurants")
+    print("3. Restaurant status")
+    print("4. Exit")
+
+def main_menu():
+    print("Fast Food\n")
+    show_options_of_the_main_menu()
+
+if __name__ == "__main__":
+    main_menu()
