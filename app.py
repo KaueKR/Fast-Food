4533e34e989864d840b5d9eaa4a7cd4a6c990823
KaueKR <kauekruiz@gmail.com>
2025-11-04 17:37:57 -0300

    Created two functions that will display the main menu

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 def show_options_of_the_main_menu():
     print("1. Register restaurant")
-    print("2. List restaurants")
+    print("2. List restaurant")
     print("3. Restaurant status")
     print("4. Exit")
 
 def main_menu():
-    print("Fast Food\n")
     show_options_of_the_main_menu()
 
 if __name__ == "__main__":
